d5a5cf644d22c78271ce7bc24ea76dd5bd8b09d6
Thirfir <thirfir@naver.com>
2026-01-09 12:04:11 +0900

    feat: Compose 통합 컨벤션 플러그인 구현

---
+++ build-logic/src/main/kotlin/plugins/AndroidComposeCommonPlugin.kt
@@ -0,0 +1,25 @@
+package plugins
+
+import extensions.catalog
+import extensions.implementation
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidComposeCommonPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("org.jetbrains.kotlin.plugin.compose")
+                apply("org.jetbrains.kotlin.plugin.serialization")
+            }
+
+            dependencies {
+                implementation(platform(catalog.findLibrary("androidx-compose-bom").get()))
+                implementation(catalog.findBundle("bundle-compose").get())
+                implementation(catalog.findBundle("bundle-android-navigation3").get())
+            }
+        }
+    }
+}
