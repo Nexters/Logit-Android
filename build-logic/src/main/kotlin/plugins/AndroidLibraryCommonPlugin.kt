553585cda983b0c5abbb43c49ac8b010a352cbcb
Thirfir <sangbbang31@gmail.com>
2026-01-11 20:20:55 +0900

    chore: 안드로이드 라이브러리 모듈 buildConfig 활성화

--- build-logic/src/main/kotlin/plugins/AndroidLibraryCommonPlugin.kt
+++ build-logic/src/main/kotlin/plugins/AndroidLibraryCommonPlugin.kt
@@ -51,6 +51,10 @@ class AndroidLibraryCommonPlugin: Plugin<Project> {
                     )
                 }
             }
+
+            buildFeatures {
+                buildConfig = true
+            }
         }
     }
 }
