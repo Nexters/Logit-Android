3c642b017fad6b4373b8e085f602773c855a39f7
Thirfir <thirfir@naver.com>
2026-01-12 17:28:26 +0900

    chore: feature:chat 모듈 피쳐 플러그인 적용

--- feature/chat/build.gradle.kts
+++ feature/chat/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.logit.compose.common)
     alias(libs.plugins.logit.hilt)
     alias(libs.plugins.logit.circuit)
+    alias(libs.plugins.logit.feature.common.dependencies)
 }
 
 android {
