303fc42265b8c45d360c9145b2ef26d598048144
Thirfir <thirfir@naver.com>
2026-01-13 10:41:13 +0900

    chore: Circuit 플러그인 추가적인 의존 선언

--- build-logic/src/main/kotlin/plugins/CircuitPlugin.kt
+++ build-logic/src/main/kotlin/plugins/CircuitPlugin.kt
@@ -2,6 +2,7 @@ package plugins
 
 import extensions.catalog
 import extensions.implementation
+import extensions.ksp
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
@@ -14,8 +15,14 @@ class CircuitPlugin: Plugin<Project> {
                 apply("kotlin-parcelize")
             }
 
+            ksp {
+                arg("circuit.codegen.mode", "hilt")
+            }
+
             dependencies {
-                implementation(catalog.findLibrary("circuit").get())
+                implementation(catalog.findBundle("bundle-circuit").get())
+                implementation(catalog.findLibrary("circuit-codegen-annotations").get())
+                ksp(catalog.findLibrary("circuit-codegen").get())
             }
         }
     }
