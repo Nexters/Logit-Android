902da9264c5b3be1dcbc7deb1607781538deae49
Thirfir <sangbbang31@gmail.com>
2026-01-15 20:28:14 +0900

    chore: app모듈에 core:navigation, circuit 의존 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,10 @@ plugins {
     alias(libs.plugins.logit.application)
     alias(libs.plugins.logit.compose.common)
     alias(libs.plugins.logit.hilt)
+    alias(libs.plugins.logit.circuit)
 }
 
 dependencies {
     implementation(projects.core.designsystem)
+    implementation(projects.core.navigation)
 }
