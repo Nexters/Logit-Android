0ce00f8cfb05791a117a2b116284a444534c9acc
Thirfir <thirfir@naver.com>
2026-01-21 08:34:19 +0900

    chore: Network BuildConfig 변수 추가 : Base url, 테스트 토큰

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.api.dsl.LibraryDefaultConfig
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.logit.android.library.common)
     alias(libs.plugins.logit.hilt)
@@ -5,6 +8,10 @@ plugins {
 
 android {
     namespace = "com.useai.core.network"
+
+    defaultConfig {
+        configureBuildConfigs()
+    }
 }
 
 dependencies {
@@ -19,4 +26,17 @@ dependencies {
     implementation(libs.okhttp.event.source)
 
     implementation(libs.kotlinx.serialization.json)
+}
+
+fun LibraryDefaultConfig.configureBuildConfigs() {
+    val localProperties = Properties()
+    val localPropertiesFile: File? = rootProject.file("local.properties")
+    if (localPropertiesFile?.exists() == true)
+        localProperties.load(localPropertiesFile.inputStream())
+
+    val baseUrl = localProperties.getProperty("BASE_URL") ?: error("local.properties에 BASE_URL을 선언해주세요")
+    buildConfigField("String", "BASE_URL", "\"$baseUrl\"")
+
+    val testUserAccessToken = localProperties.getProperty("TEST_USER_ACCESS_TOKEN") ?: error("local.properties에 TEST_USER_ACCESS_TOKEN을 선언해주세요")
+    buildConfigField("String", "TEST_USER_ACCESS_TOKEN", "\"$testUserAccessToken\"")
 }
