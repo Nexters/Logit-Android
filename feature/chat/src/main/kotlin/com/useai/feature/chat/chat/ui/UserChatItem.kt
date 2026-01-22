bba1f66637f0fc3d9968cbfd7cc9de6a9a8417cd
Thirfir <thirfir@naver.com>
2026-01-22 12:32:14 +0900

    feat: 유저 채팅 메시지 UI 구현

---
+++ feature/chat/src/main/kotlin/com/useai/feature/chat/chat/ui/UserChatItem.kt
@@ -0,0 +1,62 @@
+package com.useai.feature.chat.chat.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.useai.core.designsystem.component.text.LogitMarkdownText
+import com.useai.core.designsystem.theme.LogitTheme
+import com.useai.core.model.chat.Chat
+
+private val shape by lazy {
+    RoundedCornerShape(
+        topStart = 12.dp,
+        topEnd = 2.dp,
+        bottomEnd = 12.dp,
+        bottomStart = 12.dp
+    )
+}
+
+@Composable
+internal fun UserChatItem(
+    chat: Chat,
+    modifier: Modifier = Modifier
+) {
+
+    val backgroundBubbleColor = LogitTheme.colors.gray50    // TODO 색상 최신화
+
+    Column(
+        modifier = modifier
+    ) {
+        LogitMarkdownText(
+            markdown = chat.message,
+            modifier = Modifier.drawWithCache {
+                val outline = shape.createOutline(size, layoutDirection, this)
+
+                onDrawBehind {
+                    drawOutline(
+                        outline = outline,
+                        color = backgroundBubbleColor
+                    )
+                }
+            }.padding(vertical = 8.dp, horizontal = 12.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun UserChatItemPreview(){
+    UserChatItem(
+        chat = Chat.User(
+            message = "디자인 시스템은 아니지만, 동아리에서 공통 로고 \n" +
+                "가이드를 만든 적은 있는데 이것도 괜찮을까?",
+        ),
+        modifier = Modifier
+    )
+}
