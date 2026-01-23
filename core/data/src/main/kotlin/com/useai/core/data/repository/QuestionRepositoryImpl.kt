57b8e54cf275271de18a7c99e6aa256b4d0bda53
Thirfir <sangbbang31@gmail.com>
2026-01-23 20:58:22 +0900

    feat: QuestionRepository 구현

--- core/data/src/main/kotlin/com/useai/core/data/repository/QuestionRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/useai/core/data/repository/QuestionRepositoryImpl.kt
@@ -1,9 +1,58 @@
 package com.useai.core.data.repository
 
+import com.useai.core.model.chat.Question
+import com.useai.core.network.error.runCatchingWith
+import com.useai.core.network.request.CreateQuestionRequest
+import com.useai.core.network.request.UpdateQuestionRequest
+import com.useai.core.network.response.toQuestion
 import com.useai.core.network.source.QuestionRemoteDataSource
 import javax.inject.Inject
 
 internal class QuestionRepositoryImpl @Inject constructor(
     private val questionRemoteDataSource: QuestionRemoteDataSource
 ) : QuestionRepository {
+
+    override suspend fun createQuestion(projectId: String, question: Question): Result<String> {
+        return runCatchingWith {
+            questionRemoteDataSource.createQuestion(
+                projectId,
+                CreateQuestionRequest(
+                    title = question.title,
+                    maxLength = question.maxLength
+                )
+            ).questionId
+        }
+    }
+
+    override suspend fun getQuestions(projectId: String): Result<List<Question>> {
+        return runCatchingWith {
+            questionRemoteDataSource.getQuestions(projectId).map { it.toQuestion() }
+        }
+    }
+
+    override suspend fun getQuestion(projectId: String, questionId: String): Result<Question> {
+        return runCatchingWith {
+            questionRemoteDataSource.getQuestion(projectId, questionId).toQuestion()
+        }
+    }
+
+    override suspend fun updateQuestion(projectId: String, question: Question): Result<Question> {
+        return runCatchingWith {
+            questionRemoteDataSource.updateQuestion(
+                projectId,
+                question.id,
+                UpdateQuestionRequest(
+                    title = question.title,
+                    maxLength = question.maxLength,
+                    letter = question.letter
+                )
+            ).toQuestion()
+        }
+    }
+
+    override suspend fun deleteQuestion(projectId: String, questionId: String): Result<Unit> {
+        return runCatchingWith {
+            questionRemoteDataSource.deleteQuestion(projectId, questionId)
+        }
+    }
 }
