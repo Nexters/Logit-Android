77820e469c8d0306384b5577367a4920cbdde15f
Thirfir <thirfir@naver.com>
2026-01-23 11:31:51 +0900

    feat: 최상위 ChatUI Composable 정의

---
+++ feature/chat/src/main/kotlin/com/useai/feature/chat/chat/ui/ChatUI.kt
@@ -0,0 +1,34 @@
+package com.useai.feature.chat.chat.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.slack.circuit.codegen.annotations.CircuitInject
+import com.useai.feature.chat.chat.ChatScreen
+import dagger.hilt.android.components.ActivityRetainedComponent
+
+@CircuitInject(ChatScreen::class, ActivityRetainedComponent::class)
+@Composable
+fun ChatUI(state: ChatScreen.State, modifier: Modifier) {
+
+    Column(modifier = modifier) {
+        when(state) {
+            is ChatScreen.State.Chatting -> {
+                ChatChattingUI(state, Modifier.fillMaxSize())
+            }
+
+            is ChatScreen.State.Letter -> {
+                TODO("ChatLetterUI")
+            }
+
+            is ChatScreen.State.Loading -> {
+                TODO("ChatLoadingUI")
+            }
+
+            is ChatScreen.State.LoadFailed -> {
+                TODO("ChatLoadFailedUI")
+            }
+        }
+    }
+}
