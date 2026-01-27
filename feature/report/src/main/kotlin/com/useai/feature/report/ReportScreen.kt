78e45a473ef9515f853a985c28fda70df3b93731
Hyesung82 <comet_0419@naver.com>
2026-01-28 00:05:53 +0900

    feat: ReportPresenter 구현

--- feature/report/src/main/kotlin/com/useai/feature/report/ReportScreen.kt
+++ feature/report/src/main/kotlin/com/useai/feature/report/ReportScreen.kt
@@ -1,12 +1,35 @@
 package com.useai.feature.report
 
-import com.slack.circuit.runtime.CircuitUiEvent
+import androidx.compose.runtime.Composable
+import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.CircuitUiState
+import com.slack.circuit.runtime.Navigator
+import com.slack.circuit.runtime.presenter.Presenter
 import com.slack.circuit.runtime.screen.Screen
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.components.ActivityRetainedComponent
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data object ReportScreen : Screen {
-    sealed interface State : CircuitUiState
-    sealed interface Event : CircuitUiEvent
+    data object State : CircuitUiState
+}
+
+class ReportPresenter @AssistedInject constructor(
+    @Assisted private val navigator: Navigator,
+) : Presenter<ReportScreen.State> {
+    @Composable
+    override fun present(): ReportScreen.State {
+        return ReportScreen.State
+    }
+
+    @AssistedFactory
+    @CircuitInject(ReportScreen::class, ActivityRetainedComponent::class)
+    fun interface Factory {
+        fun create(
+            navigator: Navigator,
+        ): ReportPresenter
+    }
 }
