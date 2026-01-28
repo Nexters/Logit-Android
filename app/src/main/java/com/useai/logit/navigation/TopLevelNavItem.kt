08ed9f31bcad9ecdac7ff58c3c7aa755fd5199e1
Hyesung82 <comet_0419@naver.com>
2026-01-28 19:36:55 +0900

    chore: feature 별 string res 개선
    - feature 별 string res 제거 및 designsystem에 병합
    - TopLevelNavItem 변수명 개선(PAPER -> COVER_LETTER, ADD -> NEW_PROJECT)

--- app/src/main/java/com/useai/logit/navigation/TopLevelNavItem.kt
+++ app/src/main/java/com/useai/logit/navigation/TopLevelNavItem.kt
@@ -3,17 +3,13 @@ package com.useai.logit.navigation
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.slack.circuit.runtime.screen.Screen
+import com.useai.core.designsystem.R
 import com.useai.core.designsystem.icon.LogitIcons
 import com.useai.feature.add.AddScreen
 import com.useai.feature.chat.ChatScreen
 import com.useai.feature.experience.ExperienceScreen
 import com.useai.feature.home.HomeScreen
 import com.useai.feature.report.ReportScreen
-import com.useai.feature.add.R as addR
-import com.useai.feature.chat.R as chatR
-import com.useai.feature.experience.R as experienceR
-import com.useai.feature.home.R as homeR
-import com.useai.feature.report.R as reportR
 
 data class TopLevelNavItem(
     @get:DrawableRes val selectedIconId: Int,
@@ -25,8 +21,8 @@ data class TopLevelNavItem(
         fun fromScreen(screen: Screen): TopLevelNavItem? {
             return when (screen) {
                 is HomeScreen -> HOME
-                is ChatScreen -> PAPER
-                is AddScreen -> ADD
+                is ChatScreen -> COVER_LETTER
+                is AddScreen -> NEW_PROJECT
                 is ExperienceScreen -> EXPERIENCE
                 is ReportScreen -> REPORT
                 else -> null
@@ -38,35 +34,34 @@ data class TopLevelNavItem(
 val HOME = TopLevelNavItem(
     selectedIconId = LogitIcons.HomeSelected,
     unselectedIconId = LogitIcons.HomeDefault,
-    iconTextId = homeR.string.feature_home_title,
-    titleTextId = homeR.string.feature_home_title,
+    iconTextId = R.string.home_title,
+    titleTextId = R.string.home_title,
 )
 
-// TODO: 자소서 화면 이름 통일
-val PAPER = TopLevelNavItem(
+val COVER_LETTER = TopLevelNavItem(
     selectedIconId = LogitIcons.PaperSelected,
     unselectedIconId = LogitIcons.PaperDefault,
-    iconTextId = chatR.string.feature_chat_title,
-    titleTextId = chatR.string.feature_chat_title,
+    iconTextId = R.string.cover_letter_title,
+    titleTextId = R.string.cover_letter_title,
 )
 
-val ADD = TopLevelNavItem(
+val NEW_PROJECT = TopLevelNavItem(
     selectedIconId = LogitIcons.AddSelected,
     unselectedIconId = LogitIcons.AddDefault,
-    iconTextId = addR.string.feature_add_title,
-    titleTextId = addR.string.feature_add_title,
+    iconTextId = R.string.new_project_title,
+    titleTextId = R.string.new_project_title,
 )
 
 val EXPERIENCE = TopLevelNavItem(
     selectedIconId = LogitIcons.ExperienceSelected,
     unselectedIconId = LogitIcons.ExperienceDefault,
-    iconTextId = experienceR.string.feature_experience_title,
-    titleTextId = experienceR.string.feature_experience_title,
+    iconTextId = R.string.experience_title,
+    titleTextId = R.string.experience_title,
 )
 
 val REPORT = TopLevelNavItem(
     selectedIconId = LogitIcons.ReportSelected,
     unselectedIconId = LogitIcons.ReportDefault,
-    iconTextId = reportR.string.feature_report_title,
-    titleTextId = reportR.string.feature_report_title,
+    iconTextId = R.string.report_title,
+    titleTextId = R.string.report_title,
 )
