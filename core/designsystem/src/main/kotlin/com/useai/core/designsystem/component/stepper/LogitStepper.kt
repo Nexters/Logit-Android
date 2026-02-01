b65dd65f8978cb680db602db7fd441116e92df90
Hyesung82 <comet_0419@naver.com>
2026-02-01 22:36:55 +0900

    feat: 입력 폼 stepper 구현

---
+++ core/designsystem/src/main/kotlin/com/useai/core/designsystem/component/stepper/LogitStepper.kt
@@ -0,0 +1,57 @@
+package com.useai.core.designsystem.component.stepper
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.useai.core.designsystem.theme.LogitTheme
+
+@Composable
+fun LogitStepper(
+    modifier: Modifier = Modifier,
+    currentStep: String,
+    totalStep: String,
+) {
+    Box(
+        modifier = modifier
+            .width(40.dp)
+            .height(28.dp)
+            .background(
+                color = LogitTheme.colors.primary20,
+                shape = RoundedCornerShape(8.dp)
+            )
+    ) {
+        Row(
+            modifier = modifier
+                .align(Alignment.Center)
+        ) {
+            Text(
+                text = currentStep,
+                color = LogitTheme.colors.gray400,
+                style = LogitTheme.typography.body7_1,
+            )
+            Text(
+                text = "/$totalStep",
+                color = LogitTheme.colors.gray400,
+                style = LogitTheme.typography.body7_3,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun LogitStepperPreview() {
+    LogitStepper(
+        currentStep = "1",
+        totalStep = "2"
+    )
+}
