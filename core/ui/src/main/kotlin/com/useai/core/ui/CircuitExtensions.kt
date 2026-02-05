65ef08a874818e1cc6d141d64b3770cf2b7344d7
Thirfir <sangbbang31@gmail.com>
2026-02-05 19:24:42 +0900

    refactor: Presenter 상태 업데이트 로직 리팩토링

--- core/ui/src/main/kotlin/com/useai/core/ui/CircuitExtensions.kt
+++ core/ui/src/main/kotlin/com/useai/core/ui/CircuitExtensions.kt
@@ -1,9 +1,14 @@
 package com.useai.core.ui
 
+import androidx.compose.runtime.ProduceStateScope
 import com.slack.circuit.runtime.CircuitUiState
 
 inline fun <reified T : CircuitUiState> CircuitUiState.runOn(block: T.() -> Unit) {
     if (this is T) {
         this.block()
     }
+}
+
+inline fun<T> ProduceStateScope<T>.reduce(block: () -> T) {
+    this.value = block()
 }
--- feature/chat/src/main/kotlin/com/useai/feature/chat/ChatPresenter.kt
+++ feature/chat/src/main/kotlin/com/useai/feature/chat/ChatPresenter.kt
@@ -3,17 +3,13 @@ package com.useai.feature.chat
 import android.content.ClipData
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.platform.Clipboard
 import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.platform.toClipEntry
 import androidx.compose.ui.util.fastMap
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.retained.produceRetainedState
-import com.slack.circuit.retained.rememberRetained
 import com.slack.circuit.runtime.internal.rememberStableCoroutineScope
 import com.slack.circuit.runtime.presenter.Presenter
 import com.useai.core.data.repository.ChattingRepository
@@ -23,6 +19,7 @@ import com.useai.core.model.chat.ChattingContent
 import com.useai.core.model.chat.ChattingHistory
 import com.useai.core.model.chat.ChattingStreaming
 import com.useai.core.model.chat.Question
+import com.useai.core.ui.reduce
 import com.useai.core.ui.runOn
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
@@ -43,146 +40,174 @@ class ChatPresenter @AssistedInject constructor(
 ) : Presenter<ChatScreen.State> {
 
     private val streamingChatStringBuilder = StringBuilder()
+    private val chattingHistories = hashMapOf<Question, ChattingHistory>()
 
     @Composable
     override fun present(): ChatScreen.State {
         val scope = rememberStableCoroutineScope()
         val localClipboard = LocalClipboard.current
 
-        val questions = rememberRetained { mutableStateListOf<Question>() }
-        var currentQuestion by rememberRetained { mutableStateOf(Question.EMPTY) }
-        var selectedCategory by rememberRetained { mutableStateOf(ChatScreenCategory.CHATTING) }
-        var userMessageInput by rememberRetained { mutableStateOf("") }
-        var isHeaderUIExpanded by rememberRetained { mutableStateOf(false) }
-        var streamingStatus by rememberRetained { mutableStateOf<ChattingStreamingStatus>(ChattingStreamingStatus.Idle) }
-        val chattingHistories = rememberRetained { mutableStateMapOf<Question, ChattingHistory>() }
-
         val state by produceRetainedState<ChatScreen.State>(ChatScreen.State.Loading) {
             questionRepository.getQuestions(screen.projectId).onSuccess { initialQuestions ->
-                questions.addAll(initialQuestions)
-                currentQuestion = initialQuestions.first()
                 initialQuestions.fastMap { question ->
                     async {
-                        chattingRepository.getChatHistory(question.id).onSuccess {
-                            chattingHistories[question] = it
+                        chattingRepository.getChatHistory(question.id).onSuccess { chattingHistory ->
+                            chattingHistories[question] = chattingHistory
                         }.onFailure {
-                            value = ChatScreen.State.LoadFailed
+                            reduce { ChatScreen.State.LoadFailed }
                         }
                     }
                 }.awaitAll()
 
                 if (value !is ChatScreen.State.LoadFailed) {
-                    value = ChatScreen.State.Success(
-                        questions = initialQuestions,
-                        currentQuestion = currentQuestion,
-                        chattingHistory = chattingHistories[currentQuestion]!!,
-                        streamingStatus = ChattingStreamingStatus.Idle,
-                        userInput = "",
-                        currentCategory = ChatScreenCategory.CHATTING,
-                        isHeaderUIExpanded = isHeaderUIExpanded
-                    ) { event ->
-                        when(event) {
-                            is ChatScreen.Event.AddQuestion -> {
+                    val initialQuestion = initialQuestions.first()
 
-                            }
-                            is ChatScreen.Event.ChangeCategory -> {
-                                selectedCategory = event.category
-                            }
-                            is ChatScreen.Event.ChangeQuestion -> {
-                                currentQuestion = event.question
-                            }
-                            is ChatScreen.Event.CopyMessage -> {
-                                scope.launch {
-                                    localClipboard.copy(event.message)
+                    reduce {
+                        ChatScreen.State.Success(
+                            questions = initialQuestions,
+                            currentQuestion = initialQuestion,
+                            chattingHistory = requireNotNull(chattingHistories[initialQuestion]),
+                            streamingStatus = ChattingStreamingStatus.Idle,
+                            userInput = "",
+                            currentCategory = ChatScreenCategory.CHATTING,
+                            isHeaderUIExpanded = false
+                        ) { event ->
+                            when(event) {
+                                is ChatScreen.Event.AddQuestion -> {
+                                    // TODO
                                 }
-                            }
-                            is ChatScreen.Event.SendMessage -> {
-                                scope.launch {
-                                    chattingRepository.startChattingStream(
-                                        questionId = currentQuestion.id,
-                                        sendingMessage = event.message
-                                    ).onStart {
-                                        userMessageInput = ""
-                                        chattingHistories[currentQuestion] = chattingHistories[currentQuestion]!!.copy(
-                                            chattings = chattingHistories[currentQuestion]!!.chattings + ChattingContent.User(
-                                                id = LocalDateTime.now().toString(),
-                                                message = event.message,
-                                                createdAt = LocalDateTime.now()
+                                is ChatScreen.Event.ChangeCategory -> {
+                                    value.runOn<ChatScreen.State.Success> {
+                                        reduce {
+                                            copy(currentCategory = event.category)
+                                        }
+                                    }
+                                }
+                                is ChatScreen.Event.ChangeQuestion -> {
+                                    value.runOn<ChatScreen.State.Success> {
+                                        reduce {
+                                            copy(
+                                                currentQuestion = event.question,
+                                                chattingHistory = requireNotNull(chattingHistories[event.question])
                                             )
-                                        )
-                                    }.catch {
-                                        streamingStatus = ChattingStreamingStatus.Error
-                                    }.collect { streaming ->
-                                        when (streaming) {
-                                            is ChattingStreaming.Streaming -> {
-                                                streamingStatus = ChattingStreamingStatus.Streaming(
-                                                    streamingChatStringBuilder.append(streaming.data).toString()
-                                                )
-                                            }
-
-                                            is ChattingStreaming.Done -> {
-                                                streamingStatus = ChattingStreamingStatus.Idle
-                                                chattingHistories[currentQuestion] = chattingHistories[currentQuestion]!!.copy(
-                                                    chattings = buildList {
-                                                        addAll(chattingHistories[currentQuestion]!!.chattings)
-                                                        add(ChattingContent.AI(
-                                                            id = streaming.chatId,
-                                                            message = streamingChatStringBuilder.toString(),
-                                                            createdAt = LocalDateTime.now(),
-                                                            isLetter = streaming.isDraft
-                                                        ))
+                                        }
+                                    }
+                                }
+                                is ChatScreen.Event.CopyMessage -> {
+                                    scope.launch {
+                                        localClipboard.copy(event.message)
+                                    }
+                                }
+                                is ChatScreen.Event.SendMessage -> {
+                                    value.runOn<ChatScreen.State.Success> {
+                                        scope.launch {
+                                            chattingRepository.startChattingStream(
+                                                questionId = currentQuestion.id,
+                                                sendingMessage = event.message
+                                            ).onStart {
+                                                value.runOn<ChatScreen.State.Success> {
+                                                    reduce {
+                                                        copy(
+                                                            userInput = "",
+                                                            chattingHistory = chattingHistory.copy(
+                                                                chattings = chattingHistory.chattings + ChattingContent.User(
+                                                                    id = LocalDateTime.now().toString(),
+                                                                    message = event.message,
+                                                                    createdAt = LocalDateTime.now()
+                                                                )
+                                                            )
+                                                        )
                                                     }
-                                                )
-                                                streamingChatStringBuilder.clear()
+                                                }
+                                            }.catch {
+                                                value.runOn<ChatScreen.State.Success> {
+                                                    reduce {
+                                                        copy(streamingStatus = ChattingStreamingStatus.Error)
+                                                    }
+                                                }
+                                            }.collect { streaming ->
+                                                when (streaming) {
+                                                    is ChattingStreaming.Streaming -> {
+                                                        value.runOn<ChatScreen.State.Success> {
+                                                            reduce {
+                                                                copy(
+                                                                    streamingStatus = ChattingStreamingStatus.Streaming(
+                                                                        streamingChatStringBuilder.append(streaming.data).toString()
+                                                                    )
+                                                                )
+                                                            }
+                                                        }
+                                                    }
+
+                                                    is ChattingStreaming.Done -> {
+                                                        value.runOn<ChatScreen.State.Success> {
+                                                            reduce {
+                                                                copy(
+                                                                    streamingStatus = ChattingStreamingStatus.Idle,
+                                                                    chattingHistory = chattingHistory.copy(
+                                                                        chattings = chattingHistory.chattings + ChattingContent.AI(
+                                                                            id = streaming.chatId,
+                                                                            message = streamingChatStringBuilder.toString(),
+                                                                            createdAt = LocalDateTime.now(),
+                                                                            isLetter = streaming.isDraft
+                                                                        )
+                                                                    )
+                                                                )
+                                                            }
+                                                        }
+                                                        streamingChatStringBuilder.clear()
+                                                    }
+                                                }
                                             }
                                         }
                                     }
                                 }
-                            }
-                            is ChatScreen.Event.UpdateLetter -> {
-                                scope.launch {
-                                    questionRepository.updateQuestion(
-                                        projectId = screen.projectId,
-                                        question = currentQuestion.copy(letter = event.letter)
-                                    ).onSuccess { question ->
-                                        if (currentQuestion.id == question.id) {
-                                            chattingHistories[question] = chattingHistories[currentQuestion]!!
-                                            chattingHistories.remove(currentQuestion)
-                                            currentQuestion = question
-                                        }
-                                        initialQuestions.indexOfFirst { it.id == question.id }.let { index ->
-                                            questions[index] = question
+                                is ChatScreen.Event.UpdateLetter -> {
+                                    value.runOn<ChatScreen.State.Success> {
+                                        scope.launch {
+                                            questionRepository.updateQuestion(
+                                                projectId = screen.projectId,
+                                                question = currentQuestion.copy(letter = event.letter)
+                                            ).onSuccess { question ->
+                                                this@ChatPresenter.chattingHistories[question] = requireNotNull(chattingHistories[currentQuestion])
+                                                this@ChatPresenter.chattingHistories.remove(currentQuestion)
+                                                reduce {
+                                                    copy(
+                                                        currentQuestion = question,
+                                                        questions = questions.map {
+                                                            if (it.id == question.id) question else it
+                                                        },
+                                                        chattingHistory = requireNotNull(chattingHistories[question])
+                                                    )
+                                                }
+                                            }
                                         }
                                     }
                                 }
-                            }
-                            is ChatScreen.Event.InputMessage -> {
-                                userMessageInput = event.message
-                            }
-                            is ChatScreen.Event.UploadExperience -> {
-                                // MVP X
-                            }
-                            is ChatScreen.Event.ExpandOrShrinkHeader -> {
-                                isHeaderUIExpanded = !isHeaderUIExpanded
+                                is ChatScreen.Event.InputMessage -> {
+                                    value.runOn<ChatScreen.State.Success> {
+                                        reduce {
+                                            copy(userInput = event.message)
+                                        }
+                                    }
+                                }
+                                is ChatScreen.Event.UploadExperience -> {
+                                    // MVP X
+                                }
+                                is ChatScreen.Event.ExpandOrShrinkHeader -> {
+                                    value.runOn<ChatScreen.State.Success> {
+                                        reduce {
+                                            copy(isHeaderUIExpanded = !isHeaderUIExpanded)
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }.onFailure {
-                value = ChatScreen.State.LoadFailed
+                reduce { ChatScreen.State.LoadFailed }
             }
-        }
-
-        state.runOn<ChatScreen.State.Success> {
-            return this.copy(
-                questions = questions,
-                currentQuestion = currentQuestion,
-                chattingHistory = chattingHistories[currentQuestion]!!,
-                userInput = userMessageInput,
-                currentCategory = selectedCategory,
-                streamingStatus = streamingStatus,
-                isHeaderUIExpanded = isHeaderUIExpanded
-            )
         }
 
         return state
