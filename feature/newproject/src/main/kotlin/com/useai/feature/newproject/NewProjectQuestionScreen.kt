51e46115f95dcd88cc0ba178ea40b2e3d27e6c5c
Hyesung82 <comet_0419@naver.com>
2026-02-07 08:48:03 +0900

    feat: 프로젝트 생성 완료 시 채팅 화면으로 이동

--- feature/newproject/src/main/kotlin/com/useai/feature/newproject/NewProjectQuestionScreen.kt
+++ feature/newproject/src/main/kotlin/com/useai/feature/newproject/NewProjectQuestionScreen.kt
@@ -1,5 +1,6 @@
 package com.useai.feature.newproject
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +13,9 @@ import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
 import com.slack.circuit.runtime.screen.Screen
+import com.useai.core.data.repository.ProjectsRepository
+import com.useai.core.model.project.NewProject
+import com.useai.core.model.project.NewQuestionData
 import com.useai.core.navigation.LocalScreenProvider
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
@@ -28,13 +32,13 @@ data class NewProjectQuestionScreen(
     val talent: String,
 ) : Screen {
     data class State(
-        val questions: List<String>,
+        val questions: List<NewQuestionData>,
         val eventSink: (Event) -> Unit,
     ) : CircuitUiState
 
     sealed interface Event : CircuitUiEvent {
         data object Back : Event
-        data class OnQuestionChange(val index: Int, val value: String) : Event
+        data class OnQuestionChange(val index: Int, val value: NewQuestionData) : Event
         data object AddQuestion : Event
         data class DeleteQuestion(val index: Int) : Event
         data object CreateProject : Event
@@ -44,10 +48,11 @@ data class NewProjectQuestionScreen(
 class NewProjectQuestionPresenter @AssistedInject constructor(
     @Assisted private val screen: NewProjectQuestionScreen,
     @Assisted private val navigator: Navigator,
+    private val projectsRepository: ProjectsRepository,
 ) : Presenter<NewProjectQuestionScreen.State> {
     @Composable
     override fun present(): NewProjectQuestionScreen.State {
-        var questions by rememberRetained { mutableStateOf(listOf("")) }
+        var questions by rememberRetained { mutableStateOf(listOf(NewQuestionData())) }
         val scope = rememberCoroutineScope()
         val screenProvider = LocalScreenProvider.current
 
@@ -62,7 +67,7 @@ class NewProjectQuestionPresenter @AssistedInject constructor(
                     questions = newList
                 }
                 NewProjectQuestionScreen.Event.AddQuestion -> {
-                    questions = questions + ""
+                    questions = questions + NewQuestionData()
                 }
                 is NewProjectQuestionScreen.Event.DeleteQuestion -> {
                     val newList = questions.toMutableList()
@@ -74,10 +79,22 @@ class NewProjectQuestionPresenter @AssistedInject constructor(
                         val basicInfo = screen
                         val projectQuestions = questions
 
-                        // TODO: 서버에 프로젝트 생성 요청
-                        val projectId = ""
-                        val chatScreen = screenProvider.chatScreen(projectId)
-                        navigator.goTo(chatScreen)
+                        projectsRepository.createNewProject(
+                            newProject = NewProject(
+                                companyName = basicInfo.companyName,
+                                jobName = basicInfo.jobName,
+                                jobDesc = basicInfo.jobDesc,
+                                talent = basicInfo.talent,
+                                questions = projectQuestions,
+                                dueDate = "2024-12-31" // TODO: due date 입력 폼 추가
+                            )
+                        ).onSuccess {
+                            val projectId = it.projectId
+                            val chatScreen = screenProvider.chatScreen(projectId)
+                            navigator.goTo(chatScreen)
+                        }.onFailure {
+                            Log.e(TAG, "createProject failed: $it")
+                        }
                     }
                 }
             }
@@ -91,5 +108,9 @@ class NewProjectQuestionPresenter @AssistedInject constructor(
             screen: NewProjectQuestionScreen,
             navigator: Navigator,
         ): NewProjectQuestionPresenter
+    }
+
+    companion object {
+        private val TAG = NewProjectQuestionPresenter::class.java.simpleName
     }
 }
