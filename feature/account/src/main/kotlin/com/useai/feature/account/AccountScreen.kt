fade407c584961f5a09ff3fdee0cd189834efd9b
Hyesung82 <comet_0419@naver.com>
2026-02-18 18:52:52 +0900

    feat: 사용자 정보 화면 UI 구현

--- feature/account/src/main/kotlin/com/useai/feature/account/AccountScreen.kt
+++ feature/account/src/main/kotlin/com/useai/feature/account/AccountScreen.kt
@@ -1,7 +1,12 @@
 package com.useai.feature.account
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.slack.circuit.codegen.annotations.CircuitInject
+import com.slack.circuit.retained.rememberRetained
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.Navigator
@@ -16,10 +21,18 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 class AccountScreen : Screen {
     data class State(
+        val userName: String,
+        val reportNotificationEnabled: Boolean,
         val eventSink: (Event) -> Unit = {},
     ) : CircuitUiState
 
-    sealed interface Event : CircuitUiEvent
+    sealed interface Event : CircuitUiEvent {
+        data object Back : Event
+        data object ReportNotificationSettingUpdated : Event
+        data object Contact : Event
+        data object Logout : Event
+        data object Withdraw : Event
+    }
 }
 
 class AccountPresenter @AssistedInject constructor(
@@ -27,7 +40,40 @@ class AccountPresenter @AssistedInject constructor(
 ) : Presenter<AccountScreen.State> {
     @Composable
     override fun present(): AccountScreen.State {
-        return AccountScreen.State()
+        var reportNotificationEnabled by rememberRetained { mutableStateOf(false) }
+
+        return AccountScreen.State(
+            userName = "로짓", // TODO: Google 사용자 이름 가져오기
+            reportNotificationEnabled = reportNotificationEnabled,
+        ) { event ->
+            when (event) {
+                AccountScreen.Event.Back -> navigator.pop()
+
+                is AccountScreen.Event.ReportNotificationSettingUpdated -> {
+                    val enabled = reportNotificationEnabled.not()
+                    Log.d(TAG, "ReportNotificationSettingUpdated: $enabled")
+                    reportNotificationEnabled = enabled
+
+                    if (enabled) {
+                        // TODO: 커리어 리포트 업데이트 알림 표시 On
+                    } else {
+                        // TODO: 커리어 리포트 업데이트 알림 표시 Off
+                    }
+                }
+
+                AccountScreen.Event.Contact -> {
+                    // TODO: 문의하기
+                }
+
+                AccountScreen.Event.Logout -> {
+                    // TODO: 로그아웃
+                }
+
+                AccountScreen.Event.Withdraw -> {
+                    // TODO: 회원탈퇴
+                }
+            }
+        }
     }
 
     @AssistedFactory
@@ -36,5 +82,9 @@ class AccountPresenter @AssistedInject constructor(
         fun create(
             navigator: Navigator,
         ): AccountPresenter
+    }
+
+    companion object {
+        private val TAG = AccountPresenter::class.simpleName
     }
 }
