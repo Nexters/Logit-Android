132783dbbb8de0682bdb5a078148f39164c8e2b3
Hyesung82 <comet_0419@naver.com>
2026-02-20 00:01:02 +0900

    feat: AppHeader 일반화

--- feature/home/src/main/kotlin/com/useai/feature/home/ui/Home.kt
+++ feature/home/src/main/kotlin/com/useai/feature/home/ui/Home.kt
@@ -1,37 +1,25 @@
 package com.useai.feature.home.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.useai.core.designsystem.R
-import com.useai.core.designsystem.component.button.LogitPrimaryButton
 import com.useai.core.designsystem.theme.LogitTheme
 import com.useai.core.model.project.ProjectListItem
 import com.useai.core.ui.AppHeader
@@ -56,7 +44,19 @@ fun Home(
     ) {
         item {
             AppHeader(
-                onClickProfile = {
+                title = {
+                    Image(
+                        painter = painterResource(R.drawable.ic_symbol_word),
+                        contentDescription = stringResource(R.string.content_description_app_logo),
+                        modifier = Modifier
+                            .height(28.dp)
+                            .width(85.dp),
+                    )
+                },
+                iconPainter = painterResource(R.drawable.ic_app_user),
+                iconDescription = stringResource(R.string.content_description_user_profile),
+                iconSize = dimensionResource(R.dimen.app_header_user_profile_image_size),
+                onIconClick = {
                     state.eventSink(HomeScreen.Event.AccountClicked)
                 }
             )
