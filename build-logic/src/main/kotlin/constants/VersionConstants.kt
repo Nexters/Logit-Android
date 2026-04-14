d3cad5f26ae4ee5c14618023d289ef7b6b8443d5
Hyesung82 <comet_0419@naver.com>
2026-04-14 22:22:14 +0900

    chore: 버전 정보 업데이트 (1.0.1 -> 1.0.2)

--- build-logic/src/main/kotlin/constants/VersionConstants.kt
+++ build-logic/src/main/kotlin/constants/VersionConstants.kt
@@ -2,6 +2,6 @@ package constants
 
 private const val MAJOR_VERSION = 1
 private const val MINOR_VERSION = 0
-private const val PATCH_VERSION = 1
+private const val PATCH_VERSION = 2
 internal const val VERSION_CODE = (MAJOR_VERSION * 10000) + (MINOR_VERSION * 100) + PATCH_VERSION
 internal const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
