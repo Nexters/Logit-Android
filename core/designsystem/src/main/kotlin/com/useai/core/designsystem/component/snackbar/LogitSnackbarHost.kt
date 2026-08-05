7840f71650d710514c06cd4f63cc251124786c72
Hyesung82 <comet_0419@naver.com>
2026-08-05 20:40:06 +0900

    style: LogitSnackbarHost 컴포넌트에 Preview 추가
    - `LogitSnackbarHost` 디자인 확인을 위한 `TokenSnackbarHostPreview` Compose 프리뷰 추가

--- core/designsystem/src/main/kotlin/com/useai/core/designsystem/component/snackbar/LogitSnackbarHost.kt
+++ core/designsystem/src/main/kotlin/com/useai/core/designsystem/component/snackbar/LogitSnackbarHost.kt
@@ -1,14 +1,24 @@
 package com.useai.core.designsystem.component.snackbar
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.SnackbarVisuals
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.useai.core.designsystem.R
+import com.useai.core.designsystem.theme.LogitTheme
 
 private data class LogitSnackbarVisuals(
     override val message: String,
@@ -77,6 +87,32 @@ fun LogitSnackbarHost(
             LogitSnackbar(
                 message = snackbarData.visuals.message,
                 iconResId = iconResId,
+            )
+        }
+    }
+}
+
+@Preview(name = "Token Snackbar", showBackground = true)
+@Composable
+private fun TokenSnackbarHostPreview() {
+    LogitTheme {
+        val hostState = remember { SnackbarHostState() }
+        val message = stringResource(R.string.token_attendance_banner, 3)
+
+        LaunchedEffect(hostState, message) {
+            hostState.showLogitSnackbar(
+                message = message,
+                iconResId = R.drawable.ic_complete,
+                duration = SnackbarDuration.Indefinite,
+            )
+        }
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            LogitSnackbarHost(
+                hostState = hostState,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(20.dp),
             )
         }
     }
